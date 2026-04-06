6f69c15c9575d5263a7820a762139cf77954cb79
extazy <nazargudakov@google.com>
2026-04-06 15:59:43 +0300

    fix git 2

--- It/svlex/target/debug/deps/lazy_static-301017d5534db154.d
+++
@@ -1,6 +0,0 @@
-/home/extazy/It/svlex/target/debug/deps/lazy_static-301017d5534db154.d: /home/extazy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/lib.rs /home/extazy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/inline_lazy.rs
-
-/home/extazy/It/svlex/target/debug/deps/liblazy_static-301017d5534db154.rmeta: /home/extazy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/lib.rs /home/extazy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/inline_lazy.rs
-
-/home/extazy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/lib.rs:
-/home/extazy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/inline_lazy.rs:
--- It/svlex/target/debug/svlex.d
+++
@@ -1,1 +0,0 @@
-/home/extazy/It/svlex/target/debug/svlex: /home/extazy/It/svlex/src/ir.rs /home/extazy/It/svlex/src/lex.rs /home/extazy/It/svlex/src/main.rs /home/extazy/It/svlex/src/netlist.rs /home/extazy/It/svlex/src/parser.rs /home/extazy/It/svlex/src/semantic.rs
